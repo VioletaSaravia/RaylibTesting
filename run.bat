70c6606bfdaeaaf813885e75a1f7843ca4d5e6db
Violeta Saravia <saraviavioleta@gmail.com>
2025-06-13 21:08:05 -0300

    Fixed setup script

--- run.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set WIN32_DEBUGFLAGS=/DDEBUG /MP /W1 /Zi /EHsc /nologo
-
-set GAME=%1
-
-build.bat %GAME% win32 debug
-cl engine\main_debug.cpp /std:c++20 %WIN32_DEBUGFLAGS% /Fogames\\%GAME%\\build\\debug\\ /Fdgames\\%GAME%\\build\\debug\\ /Fegames\\%GAME%\\build\\debug\\ /link /OUT:games\\%GAME%\\build\\debug\\main_debug.exe
